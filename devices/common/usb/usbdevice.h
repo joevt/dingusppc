145e641a80dba4f724592b5ecf4f94c2b6a9c242
joevt <joevt@shaw.ca>
2023-06-14 21:59:38 -0700

    usbohci: Add USB OHCI controller.

---
+++ devices/common/usb/usbdevice.h
@@ -0,0 +1,43 @@
+/*
+DingusPPC - The Experimental PowerPC Macintosh emulator
+Copyright (C) 2018-26 The DingusPPC Development Team
+          (See CREDITS.MD for more details)
+
+(You may also contact divingkxt or powermax2286 on Discord)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef USB_DEVICE_H
+#define USB_DEVICE_H
+
+#include <devices/common/hwcomponent.h>
+
+#include <cinttypes>
+#include <string>
+
+class USBDevice : virtual public HWComponent {
+public:
+    USBDevice(const std::string name);
+    ~USBDevice() = default;
+
+protected:
+
+private:
+
+protected:
+
+};
+
+#endif // USB_DEVICE_H
