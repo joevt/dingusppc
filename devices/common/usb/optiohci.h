7d11315f80b68990b70b77f97771abde94d28b0a
joevt <joevt@shaw.ca>
2023-06-14 22:01:43 -0700

    optiohci: Add OPTi Inc 82C861 USB OHCI controller.
    This is the USB host controller used in Yosemite B&W G3.

---
+++ devices/common/usb/optiohci.h
@@ -0,0 +1,37 @@
+/*
+DingusPPC - The Experimental PowerPC Macintosh emulator
+Copyright (C) 2018-26 The DingusPPC Development Team
+          (See CREDITS.MD for more details)
+
+(You may also contact divingkxt or powermax2286 on Discord)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPTI_OHCI_H
+#define OPTI_OHCI_H
+
+#include <devices/common/usb/usbohci.h>
+
+class OptiOhci : public USBHostOHCI {
+public:
+    OptiOhci(const std::string &dev_name);
+    ~OptiOhci() = default;
+
+    static std::unique_ptr<HWComponent> create(const std::string &dev_name) {
+        return std::unique_ptr<OptiOhci>(new OptiOhci(dev_name));
+    }
+};
+
+#endif // OPTI_OHCI_H
