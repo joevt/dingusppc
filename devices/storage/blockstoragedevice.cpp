2431387c344e515c440f09f6306c4f8edd565be0
joevt <joevt@shaw.ca>
2023-07-28 14:08:46 -0700

    Log block storage device read offsets.

--- devices/storage/blockstoragedevice.cpp
+++ devices/storage/blockstoragedevice.cpp
@@ -23,6 +23,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <devices/storage/blockstoragedevice.h>
 
+#include <loguru.hpp>
+
 #include <cstring>
 
 using namespace std;
@@ -109,6 +111,10 @@ int BlockStorageDevice::read_begin(int nblocks, uint32_t max_len) {
         this->remain_size = 0;
     }
 
+    if (read_size % this->block_size)
+        LOG_F(ERROR, "read_begin %lld %d %d %d", (long long)this->cur_fpos, read_size, nblocks * this->block_size, max_len);
+    else
+        LOG_F(WARNING, "read_begin %lld %d %d %d", (long long)this->cur_fpos, read_size, nblocks * this->block_size, max_len);
     this->fill_cache(read_size / this->block_size);
 
     return read_size;
@@ -127,6 +133,10 @@ int BlockStorageDevice::read_more() {
         this->remain_size = 0;
     }
 
+    if (read_size % this->block_size)
+        LOG_F(ERROR, "read_more %lld %d", (long long)this->cur_fpos, read_size);
+    else
+        LOG_F(WARNING, "read_more %lld %d", (long long)this->cur_fpos, read_size);
     this->fill_cache(read_size / this->block_size);
 
     return read_size;
