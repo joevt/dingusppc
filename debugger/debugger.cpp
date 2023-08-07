ca9450697811f3ac44ec4c5ad27a97c0221969b7
joevt <joevt@shaw.ca>
2023-08-07 13:16:55 -0700

    debugger: Show Open Firmware word names.
    For disassembly and stepping during Open Firmware.
    
    A negative offset indicates that the address is inside the header of an
    Open Firmware word. The header may include padding after the name to
    align code at an 8 byte boundary.
    
    A positive offset may extend beyond the end of an Open Firmware word.
    The word may be followed by zero padding to align the next header on an
    8 byte boundary. Or the word might be followed by unnamed data or named
    properties or devices. No effort is made to check for property or device
    names since they are not code.
    
    The method for detecting Open Firmware context could be improved.
    
    In the future, the get_name function could be changed to handle other
    contexts and get names from different sources such as the symbol table
    or debug symbols of a binary like mach_kernel. A method for detecting or
    switching the context could also be added.
    
    A future modification could include naming of effective addresses for
    branches. The get_name function could be made a function pointer in the
    PPCDisasmContext so that the disassembler can use the function.

--- debugger/debugger.cpp
+++ debugger/debugger.cpp
@@ -425,9 +425,159 @@ static void patch_mem(string& params) {
     }
 }
 
+enum {
+    fdefd       = 0x80,
+    fimm        = 0x40,
+    fnohdr      = 0x20,
+    falias      = 0x10,
+    finstance   = 0x08,
+    fvisible    = 0x04,
+    finvisible  = 0x02,
+    fvectored   = 0x01,
+};
+
+enum {
+    ctype_colon      = 0xb7,
+    ctype_value      = 0xb8,
+    ctype_variable   = 0xb9,
+    ctype_constant   = 0xba,
+    ctype_create     = 0xbb,
+    ctype_defer      = 0xbc,
+    ctype_buffer     = 0xbd,
+    ctype_field      = 0xbe,
+    ctype_code       = 0xbf,
+    ctype_settoken   = 0xdb,
+};
+
+static std::string get_name(uint32_t addr, uint32_t addr_p) {
+    std::string str;
+
+/*
+    if (addr == 0x004097B0) {
+        LOG_F(WARNING, "you are here");
+    }
+*/
+
+    static uint32_t start_vector_ptr_saved = 0;
+    static uint32_t start_vector_ptr_saved_p = 0;
+
+    do {
+        uint32_t start_vector_ptr = (uint32_t)get_reg("r25");
+        if ((int32_t)(start_vector_ptr) >= 0)
+            break;
+        uint32_t here = (uint32_t)get_reg("r16");
+        if ((int32_t)(start_vector_ptr) >= 0)
+            break;
+
+        if (addr == addr_p) {
+            if (start_vector_ptr != start_vector_ptr_saved)
+                break;
+            here = here - start_vector_ptr + start_vector_ptr_saved_p;
+            start_vector_ptr = start_vector_ptr_saved_p;
+        }
+
+        if (start_vector_ptr <= addr && addr < here) {
+            uint32_t begin_addr;
+            uint32_t prev_begin_addr;
+
+            uint32_t  h_link;
+            uint8_t   h_flags;
+            uint8_t   h_ctype;
+            uint16_t  h_token;
+            uint64_t  h_name[256/8+1]; // includes h_count
+            
+            uint64_t val;
+
+            begin_addr = addr & ~7;
+            while (begin_addr >= start_vector_ptr) {
+                try {
+                    val = mem_read_dbg(begin_addr, 8);
+                } catch (invalid_argument& exc) {
+                    cout << "get_name caused exception: " << exc.what() << endl;
+                    break;
+                }
+                h_link = val >> 32;
+                prev_begin_addr = 0;
+                if ((h_link & 0xFFF00007) == 0xFFF00000)
+                    prev_begin_addr = begin_addr + h_link;
+                if (h_link == 0 ||
+                    (start_vector_ptr <= prev_begin_addr && prev_begin_addr <= begin_addr)
+                ) { // multiple of 8 and less than a megabyte
+                    h_flags = val >> 24;
+                    if (h_flags & fdefd) {
+                        h_ctype = val >> 16;
+                        if ((h_ctype >= ctype_colon && h_ctype <= ctype_code) || h_ctype == ctype_settoken) {
+                            if (h_flags & fnohdr) {
+                                h_token = val;
+                                switch (h_ctype) {
+                                    case ctype_colon    : snprintf((char*)h_name, sizeof(h_name), "colon_%x", h_token); break;
+                                    case ctype_value    : snprintf((char*)h_name, sizeof(h_name), "value_%x", h_token); break;
+                                    case ctype_variable : snprintf((char*)h_name, sizeof(h_name), "variable_%x", h_token); break;
+                                    case ctype_constant : snprintf((char*)h_name, sizeof(h_name), "constant_%x", h_token); break;
+                                    case ctype_create   : snprintf((char*)h_name, sizeof(h_name), "create_%x", h_token); break;
+                                    case ctype_defer    : snprintf((char*)h_name, sizeof(h_name), "defer_%x", h_token); break;
+                                    case ctype_buffer   : snprintf((char*)h_name, sizeof(h_name), "buffer_%x", h_token); break;
+                                    case ctype_field    : snprintf((char*)h_name, sizeof(h_name), "field_%x", h_token); break;
+                                    case ctype_code     : snprintf((char*)h_name, sizeof(h_name), "code_%x", h_token); break;
+                                    case ctype_settoken : snprintf((char*)h_name, sizeof(h_name), "settoken_%x", h_token); break;
+                                }
+                                str = (char*)h_name;
+                                begin_addr = begin_addr + 8;
+                            }
+                            else {
+                                unsigned int i = 0;
+                                do {
+                                    try {
+                                        val = mem_read_dbg(begin_addr + 8 + i, 8);
+                                    }
+                                    catch (invalid_argument& exc) {
+                                        cout << "get_name caused exception while getting name: " << exc.what() << endl;
+                                        val = 0;
+                                    }
+                                    WRITE_QWORD_BE_A(&(((char*)h_name)[i]), val);
+                                    i += 8;
+                                } while (i <= *(char*)h_name);
+                                ((char*)h_name)[i] = '\0';
+                                str = (((char*)h_name) + 1);
+                                begin_addr = begin_addr + 8 + i;
+                            }
+
+                            char offset[20];
+                            if (addr != begin_addr) {
+                                snprintf(offset, sizeof(offset), "%+-6d", int32_t(addr - begin_addr));
+                                str += offset;
+                            }
+                            else {
+                                str += "      ";
+                            }
+
+                            if (start_vector_ptr_saved == 0) {
+                                start_vector_ptr_saved = start_vector_ptr;
+                                mmu_translate_dbg(start_vector_ptr, start_vector_ptr_saved_p);
+                            }
+                            break;
+                        } // if ctype is valid
+                    } // if defined
+                } // if hlink is multiple of 8 and less than a megabyte
+                begin_addr -= 8;
+            } // while begin_addr >= start_vector_ptr
+        } // if addr between startvec and here
+
+    } while (0);
+    return str;
+}
+
 static uint32_t disasm(PPCDisasmContext &ctx) {
-    ctx.instr_code = READ_DWORD_BE_A(mmu_translate_imem(ctx.instr_addr));
+    uint32_t phys_addr;
+    ctx.instr_code = READ_DWORD_BE_A(mmu_translate_imem(ctx.instr_addr, &phys_addr));
     cout << COUT08X << ctx.instr_addr;
+    if (phys_addr != ctx.instr_addr) {
+        cout << "->" << COUT08X << phys_addr;
+    }
+    std::string name = get_name(ctx.instr_addr, phys_addr);
+    if (!name.empty()) {
+        cout << " " << setw(27) << left << setfill(' ') << name;
+    }
     cout << ": " << COUT08X << ctx.instr_code;
     cout << "    " << disassemble_single(&ctx) << setfill(' ') << right << dec;
     return ctx.instr_addr;
