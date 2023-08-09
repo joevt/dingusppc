aa3fc66dbc66b63941f54ed7816aaa67a607505e
joevt <joevt@shaw.ca>
2023-08-09 15:35:20 -0700

    dma: Log unsupported registers once.
    For debugging. It's not going to get supported later, so no need to spam
    the log.

--- devices/common/dbdma.h
+++ devices/common/dbdma.h
@@ -167,6 +167,9 @@ private:
     // Interrupt related stuff
     InterruptCtrl* int_ctrl = nullptr;
     uint64_t       irq_id   = 0;
+
+    uint64_t unsupported_register_read = 0;
+    uint64_t unsupported_register_write = 0;
 };
 
 #endif // DB_DMA_H
