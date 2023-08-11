091865e228b3ea6806772f134a1eb661754d97f3
joevt <joevt@shaw.ca>
2023-08-11 01:43:02 -0700

    debugger: Add symbols and gdbshowallkmods.
    So the debugger disassembler can show the names & offsets for addresses.
    The gdbshowallkmods commmand shows a list of all loaded kexts for Mac OS
    X.
    Without a symbols files, the dingussppc disassembler can still show
    segment and section name for kexts.
    
    Use -s or --symbols to specify a symbols file.
    A script is used to produce the symbols file. Example:
    
    00000000 000017b4 0 kext com.apple.driver.HeathrowATA
    00000000 000017b4 1
    00000000 00000ef4 2 __TEXT:__text
    00000000 00000000 3 HeathrowATA::HeathrowATA(OSMetaClass const*)
    00000098 00000000 3 HeathrowATA::~HeathrowATA()
    00000138 00000000 3 HeathrowATA::getMetaClass() const
    
    start: start address
    end: end address; For symbols, it may be 0 which is automatically
         adjusted to the start of the next symbol or the end of the section
         or segment or binary.
    type: 0:binary 1:segment 2:section 3:symbol
    kind: kext or kernel (may add process in the future)
    name: binary: mach_kernel or kext identifier
          segments: may be empty
          sections: contains segment name, colon, and section name
          symbol: demangled name
    
    Currently all symbols for multiple binaries are extracted to a single
    file.
    This should be changed to accept multiple symbol files.
    
    Instead of using a script, DingusPPC should be able to be given a list
    of binaries and extract the symbols directly but DingusPPC would need to
    be able to do all the things that the script does (parsing of macho-o,
    symbol table, plist (xml), and C++ mangled names). Other OSes will
    require parsing of PEF or ELF or XCOFF or other binary formats.

---
+++ debugger/symbolsopenfirmware.h
@@ -0,0 +1,30 @@
+/*
+DingusPPC - The Experimental PowerPC Macintosh emulator
+Copyright (C) 2018-26 The DingusPPC Development Team
+          (See CREDITS.MD for more details)
+
+(You may also contact divingkxt or powermax2286 on Discord)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SYMBOLS_OPENFIRMWARE_H_
+#define SYMBOLS_OPENFIRMWARE_H_
+
+#include <string>
+
+std::string get_name_OpenFirmware(uint32_t addr, uint32_t addr_p, int *offset, bool append_offset = true);
+bool lookup_name_OpenFirmware(const std::string &name, uint32_t &addr);
+
+#endif // SYMBOLS_OPENFIRMWARE_H_
