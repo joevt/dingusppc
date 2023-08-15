a0074615932f83685a305d5c66c464a867100f90
joevt <joevt@shaw.ca>
2023-08-15 14:35:20 -0700

    ppcmmu: Don't log access to unmapped memory.
    While scanning memory for structures (Open Firmware names, kmod_info, etc), don't log accesses to unmapped memory.
    Perhaps the message should be logged if explicitly accessing a memory location, such as during dump or disassemble.

--- cpu/ppc/ppcmmu.cpp
+++ cpu/ppc/ppcmmu.cpp
@@ -1,6 +1,6 @@
 /*
 DingusPPC - The Experimental PowerPC Macintosh emulator
-Copyright (C) 2018-23 divingkatae and maximum
+Copyright (C) 2018-25 divingkatae and maximum
                       (theweirdo)     spatium
 
 (Contact divingkatae#1017 or powermax#2286 on Discord for more info)
@@ -636,17 +636,45 @@ static TLBEntry* dtlb2_refill(uint32_t guest_va, int is_write, bool is_dbg = fal
         tlb_entry->phys_tag = phys_addr & ~0xFFFUL;
         return tlb_entry;
     } else {
-        if (!is_dbg) {
-        static uint32_t last_phys_addr = -1;
-        static uint32_t first_phys_addr = -1;
-        if (phys_addr != last_phys_addr + 4) {
-            if (last_phys_addr != -1 && last_phys_addr != first_phys_addr) {
-                LOG_F(WARNING, "                                                         ... phys_addr=0x%08X", last_phys_addr);
+        if (!is_dbg && mmu_exception_handler != dbg_exception_handler
+            // PM7200 addresses
+            && ppc_state.pc != 0xfff04158
+            && ppc_state.pc != 0xfff0415c
+            && ppc_state.pc != 0xfff04160
+            && ppc_state.pc != 0xfff04164
+            && ppc_state.pc != 0xfff04184
+            && ppc_state.pc != 0xfff04188
+            && ppc_state.pc != 0xfff0419c
+            && ppc_state.pc != 0xfff041a0
+            // PM8600 addresses
+            && ppc_state.pc != 0xfff0420c
+            && ppc_state.pc != 0xfff04210
+            && ppc_state.pc != 0xfff04214
+            && ppc_state.pc != 0xfff04218
+            && ppc_state.pc != 0xfff0421c
+            && ppc_state.pc != 0xfff04220
+            && ppc_state.pc != 0xfff04224
+            && ppc_state.pc != 0xfff04228
+            && ppc_state.pc != 0xfff04334
+            && ppc_state.pc != 0xfff04338
+            && ppc_state.pc != 0xfff0433c
+            && ppc_state.pc != 0xfff04340
+            && ppc_state.pc != 0xfff04360
+            && ppc_state.pc != 0xfff04364
+            && ppc_state.pc != 0xfff04378
+            && ppc_state.pc != 0xfff0437c
+        ) {
+            static uint32_t last_phys_addr = -1;
+            static uint32_t first_phys_addr = -1;
+            if (phys_addr < last_phys_addr || phys_addr > last_phys_addr + 8) {
+                if (last_phys_addr != -1 && last_phys_addr != first_phys_addr) {
+                    LOG_F(WARNING, "                                                         ... phys_addr=0x%08X",
+                        last_phys_addr);
+                }
+                first_phys_addr = phys_addr;
+                LOG_F(WARNING, "Access to unmapped physical memory, phys_addr=0x%08X, PC=%08x", first_phys_addr, ppc_state.pc);
             }
-            first_phys_addr = phys_addr;
-            LOG_F(WARNING, "Access to unmapped physical memory, phys_addr=0x%08X", first_phys_addr);
-        }
-        last_phys_addr = phys_addr;
+            last_phys_addr = phys_addr;
         }
         return &UnmappedMem;
     }
