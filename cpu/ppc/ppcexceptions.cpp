ca03e5ae1cf38d26fd74a27d28bc0305e981a45a
joevt <joevt@shaw.ca>
2023-08-24 15:56:57 -0700

    Add debug option DBG_MMU_MODE_CHANGE.

--- cpu/ppc/ppcexceptions.cpp
+++ cpu/ppc/ppcexceptions.cpp
@@ -135,7 +135,15 @@ void ppc_exception_handler(Except_Type exception_type, uint32_t srr1_bits) {
         do_ctx_sync();
     }
 
+#ifdef DBG_MMU_MODE_CHANGE
+    uint8_t cur_mode = CurITLBMode;
+#endif
     mmu_change_mode();
+#ifdef DBG_MMU_MODE_CHANGE
+    if (CurITLBMode != cur_mode) {
+        LOG_F(ERROR, "exception %d; mmu mode changed from %d to %d.", exception_type, cur_mode, CurITLBMode);
+    }
+#endif
 
     if (exception_type != Except_Type::EXC_EXT_INT && exception_type != Except_Type::EXC_DECR) {
         longjmp(exc_env, 2); /* return to the main execution loop. */
