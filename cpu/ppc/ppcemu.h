80d6030f9936d2bc36fd130033eb9433f2ece9c1
joevt <joevt@shaw.ca>
2023-09-05 02:08:28 -0700

    Add debug option POSTPONE_DECREMENTER.

--- cpu/ppc/ppcemu.h
+++ cpu/ppc/ppcemu.h
@@ -43,6 +43,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // Uncomment this to enable logging of executed instructions.
 //#define LOG_INSTRUCTIONS
 
+// Uncomment this to postpone decrementer exceptions to code that is
+// not inside an exception handler or a lwarx atomic method.
+//#define POSTPONE_DECREMENTER
+
 /** type of compiler used during execution */
 enum EXEC_MODE:uint32_t {
     interpreter     = 0,
@@ -91,6 +95,11 @@ typedef struct struct_ppc_state {
 } SetPRS;
 
 extern SetPRS ppc_state;
+
+#ifdef POSTPONE_DECREMENTER
+extern bool in_lwarx;
+extern bool in_exception;
+#endif
 
 /** symbolic names for frequently used SPRs */
 enum SPR : int {
