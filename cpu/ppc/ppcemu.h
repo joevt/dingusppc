f77cb6f0ee950c7d45a6f56c6163593ab7440ed7
joevt <joevt@shaw.ca>
2023-12-27 23:36:45 -0800

    ppcexec: Add list of AltiVec instructions.

--- cpu/ppc/ppcemu.h
+++ cpu/ppc/ppcemu.h
@@ -424,7 +424,7 @@ void ppc_fpu_off();
 void ppc_assert_int();
 void ppc_release_int();
 
-//void ppc_opcode4();
+void ppc_opcode4();
 void ppc_opcode16();
 void ppc_opcode18();
 template <field_601 for601> void ppc_opcode19();
