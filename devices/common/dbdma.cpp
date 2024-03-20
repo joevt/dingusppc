cd80a62ab2c6cfd64a077e19ce2af03305ad9a98
joevt <joevt@shaw.ca>
2024-03-20 10:26:38 -0700

    dbdma: Allow clearing only control bits.
    Only control bits can be set and cleared by software.
    Command bits and status bits cannot be cleared.

--- devices/common/dbdma.cpp
+++ devices/common/dbdma.cpp
@@ -349,7 +349,7 @@ void DMAChannel::reg_write(uint32_t offset, uint32_t value, int size) {
     switch (offset) {
     case DMAReg::CH_CTRL:
         mask     = value >> 16;
-        new_stat = (value & mask & 0xF0FFU) | (old_stat & ~mask);
+        new_stat = (value & mask & 0xF0FFU) | (old_stat & ~(mask & 0xC0FF));
         LOG_F(DBDMA, "%s: ChannelStatus mask = 0x%04X old = 0x%04X new = 0x%04X", this->get_name().c_str(), mask, old_stat, new_stat);
 
         // update ch_stat.s0...s7 if requested (needed for interrupt generation)
