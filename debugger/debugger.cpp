20c5d4cc83a392417a5fd2a642d17a3566a88b64
joevt <joevt@shaw.ca>
2024-03-20 01:39:14 -0700

    debugger: Use FILENO defines.

--- debugger/debugger.cpp
+++ debugger/debugger.cpp
@@ -819,7 +819,7 @@ void DppcDebugger::enter_debugger() {
 
 #ifndef _WIN32
     struct winsize win_size_previous;
-    ioctl(0, TIOCGWINSZ, &win_size_previous);
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &win_size_previous);
 #endif
 
     while (1) {
@@ -880,7 +880,7 @@ void DppcDebugger::enter_debugger() {
 
     #ifndef _WIN32
                 struct winsize win_size_current;
-                ioctl(0, TIOCGWINSZ, &win_size_current);
+                ioctl(STDIN_FILENO, TIOCGWINSZ, &win_size_current);
                 if (win_size_current.ws_col != win_size_previous.ws_col || win_size_current.ws_row != win_size_previous.ws_row) {
                     win_size_previous = win_size_current;
                     if (cmd.empty()) {
