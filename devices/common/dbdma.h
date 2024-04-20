788498c8f4f6d476225d694033e03c4b12508f3f
joevt <joevt@shaw.ca>
2024-04-20 04:08:57 -0700

    dbdma: Schedule interpret_cmd.
    Have the entire dbdma program executed by scheduling each iteration in the CPU loop.
    This is done only for devices that use the dbdma program to drive data input/output (has_data_callbacks) rather than have the device drive the dbdma program.

--- devices/common/dbdma.h
+++ devices/common/dbdma.h
@@ -133,7 +133,8 @@ public:
 
 protected:
     static DMACmd* fetch_cmd(uint32_t cmd_addr, DMACmd* p_cmd, bool *is_writable);
-    uint8_t interpret_cmd(void);
+    void schedule_cmd();
+    void interpret_cmd();
     void finish_cmd();
     void xfer_quad(const DMACmd *cmd_desc, DMACmd *cmd_host);
     void update_irq();
@@ -151,6 +152,8 @@ private:
     std::function<void(void)> in_cb    = nullptr; // DMA channel in callback
     std::function<void(void)> out_cb   = nullptr; // DMA channel out callback
     std::function<void(void)> flush_cb = nullptr; // DMA channel flush callback
+    bool has_data_callbacks = false;
+    uint32_t interpret_timer_id = 0;
 
     std::atomic<uint16_t> ch_stat = 0;
     uint32_t cmd_ptr        = 0;
