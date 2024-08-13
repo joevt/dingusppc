1d8485736705662f189bb5e3b30af991707e5f39
joevt <joevt@shaw.ca>
2024-08-13 02:32:17 -0700

    pcibridgebase: Log CFG_DWORD_15 changes.

--- devices/common/pci/pcibridgebase.cpp
+++ devices/common/pci/pcibridgebase.cpp
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <core/memaccess.h>
+//#include <debugger/backtrace.h>
 #include <devices/common/pci/pcibridgebase.h>
 
 PCIBridgeBase::PCIBridgeBase(std::string name, PCIHeaderType hdr_type, int num_bars) : PCIBase(name, hdr_type, num_bars)
@@ -78,6 +79,8 @@ void PCIBridgeBase::pci_cfg_write(uint32_t reg_offs, uint32_t value, AccessDetai
         this->pci_wr_primary_bus(value & 0xFFU);
         break;
     case PCI_CFG_DWORD_15:
+        //LOG_WRITE_NAMED_CONFIG_REGISTER("PCI_CFG_DWORD_15");
+        //dump_backtrace();
         this->irq_line = value >> 24;
         this->pci_wr_bridge_control(value >> 16);
         break;
