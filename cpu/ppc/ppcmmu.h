ca5051b5aa647b8e1bd6050d707db3edef4c1da6
joevt <joevt@shaw.ca>
2024-08-27 04:10:45 -0700

    ppc: Use function pointer instead of std::function.

--- cpu/ppc/ppcmmu.h
+++ cpu/ppc/ppcmmu.h
@@ -124,8 +124,13 @@ enum TLBFlags : uint16_t {
     PTE_SET_C     = 1 << 6, // tells if C bit of the PTE needs to be updated
 };
 
-extern std::function<void(uint32_t bat_reg)> ibat_update;
-extern std::function<void(uint32_t bat_reg)> dbat_update;
+#if 0
+    typedef std::function<void(uint32_t bat_reg)> BatUpdateCallback;
+#else
+    typedef void (*BatUpdateCallback)(uint32_t bat_reg);
+#endif
+extern BatUpdateCallback ibat_update;
+extern BatUpdateCallback dbat_update;
 
 extern PPC_BAT_entry ibat_array[4];
 extern PPC_BAT_entry dbat_array[4];
