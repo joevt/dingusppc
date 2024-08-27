25e198bb61be49c39a81f360d635ba5c19a905b7
joevt <joevt@shaw.ca>
2024-08-27 01:53:44 -0700

    Include size in log messages.

--- devices/memctrl/psx.cpp
+++ devices/memctrl/psx.cpp
@@ -52,7 +52,8 @@ uint32_t PsxCtrl::read(uint32_t rgn_start, uint32_t offset, int size)
     case PsxReg::Sys_Config:
         return this->sys_config;
     default:
-        LOG_F(WARNING, "PSX: read from unsupported control register at 0x%X", offset);
+        LOG_F(WARNING, "PSX: read  @%02x.%c",
+            offset, SIZE_ARG(size));
         return 0;
     }
 }
@@ -83,7 +84,8 @@ void PsxCtrl::write(uint32_t rgn_start, uint32_t offset, uint32_t value, int siz
         this->pages_cfg[(offset >> 3) - PsxReg::Page1_Mapping] = value;
         break;
     default:
-        LOG_F(WARNING, "PSX: write to unsupported/read-only control register at 0x%X", offset);
+        LOG_F(WARNING, "PSX: write @%02x.%c = %0*x",
+            offset, SIZE_ARG(size), size * 2, value);
     };
 }
 
