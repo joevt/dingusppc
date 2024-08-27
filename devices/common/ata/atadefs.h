9aca64667be3920959fc266840250a1c8a305caf
joevt <joevt@shaw.ca>
2024-08-27 03:59:33 -0700

    Comment out unused parameters.

--- devices/common/ata/atadefs.h
+++ devices/common/ata/atadefs.h
@@ -177,7 +177,7 @@ public:
     AtaNullDevice() = default;
     ~AtaNullDevice() = default;
 
-    uint16_t read(const uint8_t reg_addr) override {
+    uint16_t read(const uint8_t /*reg_addr*/) override {
         // return all one's except DD7 if no device is present
         // DD7 corresponds to the BSY bit of the status register
         // The host should have a pull-down resistor on DD7
@@ -186,7 +186,7 @@ public:
         return 0xFF7FU;
     }
 
-    void write(const uint8_t reg_addr, const uint16_t val) override {}
+    void write(const uint8_t /*reg_addr*/, const uint16_t /*val*/) override {}
 
     // invalid device ID means no real device is present
     int get_device_id() override { return ata_interface::DEVICE_ID_INVALID; }
