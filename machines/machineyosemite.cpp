b887394a506d88aeaa67b22977e3755223f1baab
joevt <joevt@shaw.ca>
2024-09-21 04:58:54 -0700

    machineyosemite: Add Yikes machine.

--- machines/machineyosemite.cpp
+++ machines/machineyosemite.cpp
@@ -142,4 +142,10 @@ static const DeviceDescription MachineYosemite_descriptor = {
     "Power Macintosh G3 Blue and White"
 };
 
+static const DeviceDescription MachineYikes_descriptor = {
+    Machine::create<MachineYosemite>, yosemite_devices, yosemite_settings, HWCompType::MACHINE,
+    "Power Macintosh G4 Yikes"
+};
+
 REGISTER_DEVICE(pmg3nw, MachineYosemite_descriptor);
+REGISTER_DEVICE(pmyikes, MachineYikes_descriptor);
