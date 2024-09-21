f2508c6dea79555267b432785bdc393af999b047
joevt <joevt@shaw.ca>
2024-09-21 04:55:38 -0700

    necehci: HWComponent changes (for fixup).

--- devices/common/usb/necehci.cpp
+++ devices/common/usb/necehci.cpp
@@ -1,6 +1,6 @@
 /*
 DingusPPC - The Experimental PowerPC Macintosh emulator
-Copyright (C) 2018-23 divingkatae and maximum
+Copyright (C) 2018-24 divingkatae and maximum
                       (theweirdo)     spatium
 
 (Contact divingkatae#1017 or powermax#2286 on Discord for more info)
@@ -26,7 +26,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <memaccess.h>
 
 necehci::necehci()
-    : PCIDevice("necehci")
+    : PCIDevice("NecEhci"), HWComponent("NecEhci")
 {
     supports_types(HWCompType::MMIO_DEV | HWCompType::PCI_DEV);
 
