86a9346dd1b6a1327107a67b417eec73d834c4c3
joevt <joevt@shaw.ca>
2024-10-09 02:26:11 -0700

    machinefactory: Add missing help for Sixty6.

--- machines/machinefactory.cpp
+++ machines/machinefactory.cpp
@@ -442,6 +442,8 @@ const map<string, PropHelpItem> gPropHelp = {
     {"emmo",            {PropertyMachine, "enables/disables factory HW tests during startup"}},
     {"cpu",             {PropertyMachine, "specifies CPU"}},
     {"adb_devices",     {PropertyMachine, "specifies which ADB device(s) to attach"}},
+    {"has_composite",   {PropertyMachine, "indicates if composite video output is connected"}},
+    {"has_svideo",      {PropertyMachine, "indicates if s-video output is connected"}},
 };
 
 static uint32_t adler32(char *buf, size_t len) {
