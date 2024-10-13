61dc86deb82a07d1f8c04e0e4d6f4ba1588f3019
joevt <joevt@shaw.ca>
2024-10-13 05:26:05 -0700

    Add PostInitResultType enum.

--- devices/video/sixty6.h
+++ devices/video/sixty6.h
@@ -115,7 +115,7 @@ protected:
     void disable_display();
 
     // HWComponent methods
-    int device_postinit();
+    PostInitResultType device_postinit();
 
     // IobusDevice methods
     uint16_t iodev_read(uint32_t address);
