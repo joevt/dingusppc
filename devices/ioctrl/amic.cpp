9f6e58cf604260498cd8a00d79adee7b063fa3ea
joevt <joevt@shaw.ca>
2024-10-13 05:26:05 -0700

    Add PostInitResultType enum.

--- devices/ioctrl/amic.cpp
+++ devices/ioctrl/amic.cpp
@@ -100,7 +100,7 @@ AMIC::~AMIC()
     }
 }
 
-int AMIC::device_postinit()
+PostInitResultType AMIC::device_postinit()
 {
     MemCtrlBase *mem_ctrl = dynamic_cast<MemCtrlBase *>
                            (gMachineObj->get_comp_by_type(HWCompType::MEM_CTRL));
@@ -120,7 +120,7 @@ int AMIC::device_postinit()
     // set EMMO pin status (active low)
     this->emmo_pin = GET_BIN_PROP("emmo") ^ 1;
 
-    return 0;
+    return PI_SUCCESS;
 }
 
 uint32_t AMIC::read(uint32_t /*rgn_start*/, uint32_t offset, int size)
