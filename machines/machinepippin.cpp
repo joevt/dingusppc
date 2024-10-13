db44205acd8e3cc22a5e5ba23256b87ff5707889
joevt <joevt@shaw.ca>
2024-10-13 05:16:50 -0700

    adbbus: Include Adb prefix.

--- machines/machinepippin.cpp
+++ machines/machinepippin.cpp
@@ -91,7 +91,7 @@ static const PropMap Pippin_settings = {
     {"emmo",
         new BinProperty(0)},
     {"adb_devices",
-        new StrProperty("AppleJack,Keyboard")},
+        new StrProperty("AdbAppleJack,AdbKeyboard")},
 };
 
 static std::vector<std::string> Pippin_devices = {
