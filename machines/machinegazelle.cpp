efc44cf9ae95bb3d38051e6ef758d4b40731c247
joevt <joevt@shaw.ca>
2024-10-23 21:09:00 -0700

    Change irq_map vector to map.

--- machines/machinegazelle.cpp
+++ machines/machinegazelle.cpp
@@ -35,14 +35,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <memctrl/memctrlbase.h>
 #include <loguru.hpp>
 
-static std::vector<PciIrqMap> psx_irq_map = {
-    {nullptr , DEV_FUN(0x0B,0), IntSrc::BANDIT1},
-    {"pci_A1", DEV_FUN(0x0D,0), IntSrc::PCI_A},
-    {"pci_B1", DEV_FUN(0x0E,0), IntSrc::PCI_B},
-    {"pci_C1", DEV_FUN(0x0F,0), IntSrc::PCI_C},
-    {nullptr , DEV_FUN(0x10,0),              }, // OHare
-    {"pci_E1", DEV_FUN(0x11,0), IntSrc::PCI_E},
-    {"pci_F1", DEV_FUN(0x12,0), IntSrc::PCI_F},
+static std::map<int,PciIrqMap> psx_irq_map = {
+    {DEV_FUN(0x0B,0), {nullptr , IntSrc::BANDIT1}},
+    {DEV_FUN(0x0D,0), {"pci_A1", IntSrc::PCI_A  }},
+    {DEV_FUN(0x0E,0), {"pci_B1", IntSrc::PCI_B  }},
+    {DEV_FUN(0x0F,0), {"pci_C1", IntSrc::PCI_C  }},
+    {DEV_FUN(0x10,0), {nullptr ,                }}, // OHare
+    {DEV_FUN(0x11,0), {"pci_E1", IntSrc::PCI_E  }},
+    {DEV_FUN(0x12,0), {"pci_F1", IntSrc::PCI_F  }},
 };
 
 // TODO: move it to /cpu/ppc
