dc845f4c370b58efb4f97227063f30b2112c3604
joevt <joevt@shaw.ca>
2024-10-26 23:42:36 -0700

    main: Make sure gMachineObj gets deleted.

--- main.cpp
+++ main.cpp
@@ -304,6 +304,9 @@ int main(int argc, char** argv) {
         break;
     }
 
+    // if we didn't delete this then delete it now
+    delete gMachineObj.release();
+
     cleanup();
 
     return 0;
