10c0322b401b02d6e9b8f7894186b3c542983f83
joevt <joevt@shaw.ca>
2024-10-31 04:59:11 -0700

    debugger: Add fregs command.

--- cpu/ppc/ppcemu.h
+++ cpu/ppc/ppcemu.h
@@ -682,7 +682,6 @@ extern void ppc_exec_until(uint32_t goal_addr);
 extern void ppc_exec_dbg(uint32_t start_addr, uint32_t size);
 
 /* debugging support API */
-void print_fprs(void);                   /* print content of the floating-point registers  */
 uint64_t get_reg(std::string reg_name); /* get content of the register reg_name */
 void set_reg(std::string reg_name, uint64_t val); /* set reg_name to val */
 
