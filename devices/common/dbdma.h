0c98677dee8178075aa1437a9613dfee0d59da41
joevt <joevt@shaw.ca>
2024-11-09 00:45:40 -0800

    dbdma: Merge reg_read_aligned into reg_read.

--- devices/common/dbdma.h
+++ devices/common/dbdma.h
@@ -139,7 +139,6 @@ protected:
     void update_irq();
     void xfer_from_device();
     void xfer_to_device();
-    uint32_t reg_read_aligned(uint32_t offset);
 
     void start(void);
     void resume(void);
