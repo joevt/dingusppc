d386716c0c388d6233b36139e32cdb3e3236dfc6
joevt <joevt@shaw.ca>
2024-11-17 11:53:24 -0800

    dbdma: Schedule interpret_cmd again.
    Have the entire dbdma program executed by scheduling each iteration in the CPU loop.
    This is done only for devices that use the dbdma program to drive data input/output (has_data_callbacks) rather than have the device drive the dbdma program.

--- devices/common/dbdma.cpp
+++ devices/common/dbdma.cpp
@@ -52,6 +52,7 @@ void DMAChannel::set_data_callbacks(DbdmaCallback in_cb, DbdmaCallback out_cb, D
     this->in_cb    = in_cb;
     this->out_cb   = out_cb;
     this->flush_cb = flush_cb;
+    this->has_data_callbacks = in_cb || out_cb || flush_cb;
 }
 
 /* Load DMACmd from physical memory. */
@@ -69,6 +70,17 @@ DMACmd* DMAChannel::fetch_cmd(uint32_t cmd_addr, DMACmd* p_cmd, bool *is_writabl
     return cmd_host;
 }
 
+void DMAChannel::schedule_cmd() {
+    if (!interpret_timer_id) {
+        interpret_timer_id = TimerManager::get_instance()->add_oneshot_timer(1000000, [this] {
+            interpret_timer_id = 0;
+            if (this->ch_stat & CH_STAT_ACTIVE) {
+                this->interpret_cmd("schedule_cmd");
+            }
+        });
+    }
+}
+
 void DMAChannel::interpret_cmd(const char *from) {
     DMACmd cmd_struct;
     MapDmaResult res;
@@ -81,6 +93,10 @@ void DMAChannel::interpret_cmd(const char *from) {
 
         // if there is no data remaining to transfer then the command is finished and the program should procede to the next command
         this->finish_cmd();
+        if (this->has_data_callbacks) {
+            schedule_cmd();
+            return;
+        }
     }
 
     bool cmd_is_writable;
@@ -160,6 +176,11 @@ void DMAChannel::interpret_cmd(const char *from) {
         this->ch_stat &= ~CH_STAT_ACTIVE;
     }
 
+    if (this->has_data_callbacks) {
+        if (this->has_data_callbacks) {
+            schedule_cmd();
+        }
+    }
 }
 
 void DMAChannel::finish_cmd() {
