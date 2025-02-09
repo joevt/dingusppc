da7f0cc714c1aa80a96149c0492c75ad64e87d55
joevt <joevt@shaw.ca>
2025-02-09 00:34:14 -0800

    displayid: EDID changes (in progress)
    Add an EDID property.
    Fix DDC2B support.
    Allow Apple Monitor Sense and DDC to be used simultaneously.
    Add E-DDC support.

--- devices/video/displayid.cpp
+++ devices/video/displayid.cpp
@@ -31,37 +31,35 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <string>
 #include <regex>
 
+namespace loguru {
+    enum : Verbosity {
+        Verbosity_DDC = loguru::Verbosity_INFO
+    };
+}
+
 DisplayID::DisplayID(const std::string name) : HWComponent(name)
 {
     supports_types(HWCompType::DISPLAY);
-
-    /* Initialize DDC I2C bus */
-    this->next_state = I2CState::STOP;
-    this->prev_state = I2CState::STOP;
-    this->last_sda   = 1;
-    this->last_scl   = 1;
-    this->data_ptr   = 0;
-    this->data_pos   = 0;
 }
 
 HWComponent* DisplayID::set_property(const std::string &property, const std::string &value, int32_t unit_address)
 {
     if (unit_address == -1 || unit_address == 0) {
         if (this->override_property(property, value)) {
+
+            std::smatch results;
+
             if (property == "mon_id") {
-                // assume a DDC monitor is connected by default
-                this->id_kind = Disp_Id_Kind::DDC2B;
-                std_sense_code = 0;
-                ext_sense_code = 0;
+                std::string mon_id = this->get_property_str("mon_id");
+
+                bool is_apple_sense = false;
 
                 do {
-                    std::string mon_id = this->get_property_str("mon_id");
-
                     if (mon_id.empty()) {
                         this->set_name("Display_DDC");
                         break;
                     }
-                    
+
                     if (MonitorAliasToId.count(mon_id)) {
                         mon_id = MonitorAliasToId.at(mon_id);
                     }
@@ -70,11 +68,9 @@ HWComponent* DisplayID::set_property(const std::string &property, const std::str
                         this->std_sense_code = monitor.std_sense_code;
                         this->ext_sense_code = monitor.ext_sense_code;
                         this->set_name("Display_" + mon_id);
-                        this->id_kind = Disp_Id_Kind::AppleSense;
+                        is_apple_sense = true;
                         break;
                     }
-
-                    std::smatch results;
 
                     std::regex standard_sense_re("([0-7])", std::regex_constants::icase);
                     if (std::regex_match(mon_id, results, standard_sense_re)) {
@@ -84,7 +80,7 @@ HWComponent* DisplayID::set_property(const std::string &property, const std::str
                         char buf[20];
                         snprintf(buf, sizeof(buf), "Display_%d.%02X", this->std_sense_code, this->ext_sense_code);
                         this->set_name(buf);
-                        this->id_kind = Disp_Id_Kind::AppleSense;
+                        is_apple_sense = true;
                         break;
                     }
 
@@ -95,19 +91,52 @@ HWComponent* DisplayID::set_property(const std::string &property, const std::str
                         char buf[20];
                         snprintf(buf, sizeof(buf), "Display_%d.%02X", this->std_sense_code, this->ext_sense_code);
                         this->set_name(buf);
-                        this->id_kind = Disp_Id_Kind::AppleSense;
+                        is_apple_sense = true;
                         break;
                     }
                 } while(0);
 
-                if (this->id_kind == Disp_Id_Kind::AppleSense) {
-                    LOG_F(INFO, "DisplayID mode set to AppleSense");
+                if (is_apple_sense) {
+                    this->id_kind |= Disp_Id_Kind::AppleSense;
+                    LOG_F(INFO, "Added Apple Sense");
                     LOG_F(INFO, "Standard sense code: %d", this->std_sense_code);
                     LOG_F(INFO, "Extended sense code: 0x%02X", this->ext_sense_code);
+                    video_ctrl->update_display_connection();
+                    return this;
+                } else {
+                    LOG_F(ERROR, "Ignored invalid Apple Sense");
                 }
+            }
 
-                video_ctrl->update_display_connection();
-                return this;
+            if (property == "edid") {
+                std::string hex = this->get_property_str("edid");
+
+                std::regex edid_re("(?:[0-9A-F]{256}){0,255}", std::regex_constants::icase);
+                if (std::regex_match(hex, results, edid_re)) {
+                    this->edid_length = (uint16_t)(hex.length() / 2);
+                    if (edid_length) {
+                        this->edid = std::unique_ptr<uint8_t[]>(new uint8_t[edid_length]);
+                        for (int i = 0; i < edid_length; i++) {
+                            this->edid[i] = (((hex[i*2  ] & 15) + (hex[i*2  ] > '9' ? 9 : 0)) << 4) +
+                                             ((hex[i*2+1] & 15) + (hex[i*2+1] > '9' ? 9 : 0));
+                        }
+                        this->id_kind |= Disp_Id_Kind::DDC2B;
+                        this->id_kind |= Disp_Id_Kind::DDC1;
+                        if (edid_length > 256)
+                            this->id_kind |= Disp_Id_Kind::EDDC;
+                        LOG_F(INFO, "Added EDID");
+                    } else if (this->edid) {
+                        this->edid.reset();
+                        this->id_kind &= ~Disp_Id_Kind::DDC2B;
+                        this->id_kind &= ~Disp_Id_Kind::DDC1;
+                        this->id_kind &= ~Disp_Id_Kind::EDDC;
+                        LOG_F(INFO, "Removed EDID");
+                    }
+                    video_ctrl->update_display_connection();
+                    return this;
+                } else {
+                    LOG_F(ERROR, "Ignored invalid EDID");
+                }
             }
         }
     }
@@ -116,33 +145,59 @@ HWComponent* DisplayID::set_property(const std::string &property, const std::str
 
 uint8_t DisplayID::read_monitor_sense(uint8_t levels, uint8_t dirs)
 {
-    uint8_t scl, sda;
-
-    switch(this->id_kind) {
-    case Disp_Id_Kind::DDC2B:
-        /* If GPIO pins are in the output mode, pick up their levels.
-           In the input mode, GPIO pins will be read "high" */
-        scl = (dirs & 2) ? !!(levels & 2) : 1;
-        sda = (dirs & 4) ? !!(levels & 4) : 1;
-
-        return update_ddc_i2c(sda, scl);
-    case Disp_Id_Kind::AppleSense:
+    LOG_F(INFO, "dirs:%d levels:%d", dirs, levels);
+    if (this->id_kind & Disp_Id_Kind::AppleSense) {
         switch ((dirs << 3) | levels) {
-        case 0b0'100'011: // Sense line 2 pulled low; get sense line 1 and 0
-            LOG_F(INFO, "Sense line 2 pulled low; get sense line 1 and 0");
-            return  ((this->ext_sense_code & 0b0'11'00'00) >> 4);               // -> 0__
-        case 0b0'010'101: // Sense line 1 pulled low; get sense line 2 and 0
-            LOG_F(INFO, "Sense line 1 pulled low; get sense line 2 and 0");
-            return (((this->ext_sense_code & 0b0'00'10'00) >> 1) |  // -> _00
-                    ((this->ext_sense_code & 0b0'00'01'00) >> 2));  // -> 00_   // -> _0_
-        case 0b0'001'110: // Sense line 0 pulled low; get sense line 2 and 1
-            LOG_F(INFO, "Sense line 0 pulled low; get sense line 2 and 1");
-            return  ((this->ext_sense_code & 0b0'00'00'11) << 1);               // -> __0
-        default:
-            LOG_F(INFO, "dirs:%d levels:%d", dirs, levels);
-            return this->std_sense_code;
+        case 0b0'100'011: LOG_F(INFO, "Sense line 2 pulled low; get sense line 1 and 0"); break;
+        case 0b0'010'101: LOG_F(INFO, "Sense line 1 pulled low; get sense line 2 and 0"); break;
+        case 0b0'001'110: LOG_F(INFO, "Sense line 0 pulled low; get sense line 2 and 1"); break;
         }
     }
+
+    levels = apply_sense(levels, dirs);
+
+    uint8_t i2cresult = 0b0'111;
+    if (this->id_kind & Disp_Id_Kind::DDC2B) {
+        uint8_t scl, sda;
+        /* If GPIO pins are in the output mode, pick up their levels.
+           In the input mode, GPIO pins will be read "high" */
+        scl = !!(levels & 0b0'010);
+        sda = !!(levels & 0b0'100);
+
+        i2cresult = update_ddc_i2c(sda, scl);
+        levels &= 0b0'001 | i2cresult;
+        LOG_F(INFO, "levels (i2c):%d", levels);
+
+        levels = apply_sense(levels, dirs);
+    }
+
+    return levels;
+}
+
+uint8_t DisplayID::apply_sense(uint8_t levels, uint8_t dirs)
+{
+    /* If GPIO pins are in the output mode (dir=1), pick up their levels.
+       In the input mode (dir=0), GPIO pins will be read "high" unless grounded by Apple monitor sense. */
+    uint8_t new_levels = ((dirs ^ 7) | (dirs & levels)) & this->std_sense_code;
+    if (new_levels != levels) {
+        levels = new_levels;
+        LOG_F(INFO, "levels (dir):%d", levels);
+    }
+    uint8_t other[3][2] = { {2,1}, {2,0}, {1,0} }; // other sense lines for sense line 0, 1, 2
+    for (int i = 1; i >= 0; i--) {
+        for (int sense = 2; sense >= 0; sense--) { // loop sense line 2 to 0
+            if ( ((1 << sense) & dirs) && !((1 << sense) & levels) ) { // if the sense line is driven low
+                new_levels = levels;
+                new_levels &= ~( ((~this->ext_sense_code >> (sense * 2 + 1)) & 1) << other[sense][0] );
+                new_levels &= ~( ((~this->ext_sense_code >> (sense * 2 + 0)) & 1) << other[sense][1] );
+                if (new_levels != levels) {
+                    levels = new_levels;
+                    LOG_F(INFO, "levels (ext):%d", levels);
+                }
+            }
+        }
+    }
+    return levels;
 }
 
 uint8_t DisplayID::set_result(uint8_t sda, uint8_t scl)
@@ -155,11 +210,11 @@ uint8_t DisplayID::set_result(uint8_t sda, uint8_t scl)
     data_out = 0;
 
     if (scl) {
-        data_out |= 2;
+        data_out |= 0b0'010;
     }
 
     if (sda) {
-        data_out |= 4;
+        data_out |= 0b0'100;
     }
 
     return data_out;
@@ -181,11 +236,12 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
         /* STOP  = SDA goes low to high while SCL is high */
         if (this->last_scl) {
             if (!sda) {
-                LOG_F(9, "DDC-I2C: START condition detected!");
+                LOG_F(DDC, "DDC-I2C: START condition detected!");
                 this->next_state = I2CState::DEV_ADDR;
                 this->bit_count  = 0;
+                this->byte       = 0;
             } else {
-                LOG_F(9, "DDC-I2C: STOP condition detected!");
+                LOG_F(DDC, "DDC-I2C: STOP condition detected!");
                 this->next_state = I2CState::STOP;
             }
         }
@@ -198,6 +254,7 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
 
     switch (this->next_state) {
     case I2CState::STOP:
+        LOG_F(WARNING, "DDC-I2C: STOP");
         break;
 
     case I2CState::ACK:
@@ -205,7 +262,9 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
         this->byte      = 0;
         switch (this->prev_state) {
         case I2CState::DEV_ADDR:
+            LOG_F(WARNING, "DDC-I2C: ACK DEV_ADDR");
             if ((dev_addr & 0xFE) == 0xA0) {
+                LOG_F(WARNING, "DDC-I2C: got address 0x%X", this->dev_addr);
                 sda = 0; /* send ACK */
             } else {
                 LOG_F(ERROR, "DDC-I2C: unknown device address 0x%X", this->dev_addr);
@@ -213,7 +272,7 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
             }
             if (this->dev_addr & 1) {
                 this->next_state = I2CState::DATA;
-                this->data_ptr   = this->edid;
+                this->data_ptr   = &this->edid[0];
                 this->data_pos   = 0;
                 this->byte       = this->data_ptr[this->data_pos++];
             } else {
@@ -221,6 +280,7 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
             }
             break;
         case I2CState::REG_ADDR:
+            LOG_F(WARNING, "DDC-I2C: ACK REG_ADDR");
             this->next_state = I2CState::DATA;
             if (!this->reg_addr) {
                 sda = 0; /* send ACK */
@@ -230,6 +290,7 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
             }
             break;
         case I2CState::DATA:
+            LOG_F(WARNING, "DDC-I2C: ACK DATA");
             this->next_state = I2CState::DATA;
             if (dev_addr & 1) {
                 if (!sda) {
@@ -238,6 +299,7 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
                         this->byte = this->data_ptr[this->data_pos++];
                     else
                         this->byte = 0;
+                    LOG_F(WARNING, "DDC-I2C: byte[%02X]:%02X", this->data_pos - 1, this->byte);
                 } else {
                     LOG_F(ERROR, "DDC-I2C: Oops! NACK received");
                 }
@@ -245,21 +307,25 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
                 sda = 0; /* send ACK */
             }
             break;
+        default:
+            LOG_F(WARNING, "DDC-I2C: ACK STATE:%d", this->prev_state);
         }
         break;
 
     case I2CState::DEV_ADDR:
     case I2CState::REG_ADDR:
         this->byte = (this->byte << 1) | this->last_sda;
+        LOG_F(WARNING, "DDC-I2C: %s bit:%d byte:%02X",
+            this->next_state == I2CState::DEV_ADDR ? "DEV_ADDR" : "REGADDR", this->bit_count, this->byte);
         if (this->bit_count++ >= 7) {
             this->bit_count  = 0;
             this->prev_state = this->next_state;
             this->next_state = I2CState::ACK;
             if (this->prev_state == I2CState::DEV_ADDR) {
-                LOG_F(9, "DDC-I2C: device address received, addr=0x%X", this->byte);
+                LOG_F(DDC, "DDC-I2C: device address received, addr=0x%X", this->byte);
                 this->dev_addr = this->byte;
             } else {
-                LOG_F(9, "DDC-I2C: register address received, addr=0x%X", this->byte);
+                LOG_F(DDC, "DDC-I2C: register address received, addr=0x%X", this->byte);
                 this->reg_addr = this->byte;
             }
         }
@@ -267,18 +333,24 @@ uint8_t DisplayID::update_ddc_i2c(uint8_t sda, uint8_t scl)
 
     case I2CState::DATA:
         sda = (this->byte >> (7 - this->bit_count)) & 1;
+        LOG_F(WARNING, "DDC-I2C: DATA bit:%d byte:%02X", this->bit_count, this->byte);
         if (this->bit_count++ >= 7) {
             this->bit_count  = 0;
             this->prev_state = this->next_state;
             this->next_state = I2CState::ACK;
         }
         break;
+    default:
+        LOG_F(WARNING, "DDC-I2C: STATE:%d", this->next_state);
     }
 
     return set_result(sda, scl);
 }
 
-static const PropMap Display_Properties = {{"mon_id", new StrProperty("")},};
+static const PropMap Display_Properties = {
+    {"mon_id", new StrProperty("")},
+    {"edid"  , new StrProperty("")},
+};
 
 static const DeviceDescription Display_Descriptor = {
     DisplayID::create, {}, Display_Properties, HWCompType::DISPLAY
