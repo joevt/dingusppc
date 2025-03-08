cf6c015ed4d7f25fa39b5afa858e9f6ed3764e8c
joevt <joevt@shaw.ca>
2025-03-08 04:35:27 -0800

    pcibase: Add read method for expansion rom region.
    Devices that inherit from pcibase may call the parent read method to
    handle reading of the PCI expansion rom region.

--- devices/video/atirage.cpp
+++ devices/video/atirage.cpp
@@ -736,18 +736,7 @@ uint32_t ATIRage::read(uint32_t rgn_start, uint32_t offset, int size)
         return 0;
     }
 
-    // memory mapped expansion ROM region
-    if (rgn_start == this->exp_rom_addr) {
-        if (offset < this->exp_rom_size)
-            return read_mem(&this->exp_rom_data[offset], size);
-        LOG_F(WARNING, "%s: read  unmapped ROM region %08x.%c",
-            this->get_name_and_unit_address().c_str(), offset, SIZE_ARG(size));
-        return 0;
-    }
-
-    LOG_F(WARNING, "%s: read  unmapped aperture region %08x.%c",
-          this->get_name_and_unit_address().c_str(), offset, SIZE_ARG(size));
-    return 0;
+    return PCIBase::read(rgn_start, offset, size);
 }
 
 void ATIRage::write(uint32_t rgn_start, uint32_t offset, uint32_t value, int size)
