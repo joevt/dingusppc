cc86af476b9b2d10c9a010fc737887b15c9b49b1
joevt <joevt@shaw.ca>
2025-06-27 03:18:47 -0700

    adbkeyboard: Support register 2 (in progress).
    This register has modifier and LED state and can change LED state.

--- devices/common/adb/adbkeyboard.h
+++ devices/common/adb/adbkeyboard.h
@@ -46,6 +46,7 @@ public:
     void event_handler(const KeyboardEvent& event);
 
     bool get_register_0() override;
+    bool get_register_2() override;
     void set_register_2() override;
     void set_register_3() override;
 
