8e6384d9f7929023ef2aa2f7da2ec3a8bb44acad
joevt <joevt@shaw.ca>
2025-06-29 23:57:10 -0700

    ofnvram: Move --setenv handling.

--- devices/common/ofnvram.h
+++ devices/common/ofnvram.h
@@ -1,6 +1,6 @@
 /*
 DingusPPC - The Experimental PowerPC Macintosh emulator
-Copyright (C) 2018-25 divingkatae and maximum
+Copyright (C) 2018-26 divingkatae and maximum
                       (theweirdo)     spatium
 
 (Contact divingkatae#1017 or powermax#2286 on Discord for more info)
@@ -135,6 +135,9 @@ public:
     int  init();
     void printenv();
     bool setenv(std::string var_name, std::string value);
+    static void setenv_from_command_line();
+
+    static std::vector<std::string> env_vars;
 
 protected:
     bool open_container();
