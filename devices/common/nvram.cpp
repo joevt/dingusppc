a3db59b5b2db9b29c96d2c587800dab64856e625
joevt <joevt@shaw.ca>
2025-08-30 02:58:34 -0700

    nvram: Add NVRAM read/write logging.

--- devices/common/nvram.cpp
+++ devices/common/nvram.cpp
@@ -33,6 +33,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 /** @file Non-volatile RAM implementation.
  */
+namespace loguru {
+    enum : Verbosity {
+        Verbosity_NVRAM_R = loguru::Verbosity_9,
+        Verbosity_NVRAM_W = loguru::Verbosity_9,
+    };
+}
 
 using namespace std;
 
@@ -61,10 +67,13 @@ NVram::~NVram() {
 }
 
 uint8_t NVram::read_byte(uint32_t offset) {
-    return (this->storage[offset]);
+    uint8_t val = this->storage[offset];
+    LOG_F(NVRAM_R, "nvram read  %04x = %02x", offset, val);
+    return (val);
 }
 
 void NVram::write_byte(uint32_t offset, uint8_t val) {
+    LOG_F(NVRAM_W, "nvram write %04x = %02x", offset, val);
     this->storage[offset] = val;
 }
 
