baddc6f9b9653fdd552a8271dfea36692a7e888f
joevt <joevt@shaw.ca>
2025-11-28 03:57:05 -0800

    ppcmmu: Don't log memory probing.
    This table should be moved to romidentity or bootrom class.

--- cpu/ppc/ppcmmu.cpp
+++ cpu/ppc/ppcmmu.cpp
@@ -672,7 +672,34 @@ static TLBEntry* dtlb2_refill(uint32_t guest_va, int is_write, bool is_dbg = fal
         tlb_entry->phys_tag = phys_addr & ~0xFFFUL;
         return tlb_entry;
     } else {
-        if (!is_dbg && mmu_exception_handler != dbg_exception_handler) {
+        if (!is_dbg && mmu_exception_handler != dbg_exception_handler
+            // PM7200 addresses
+            && ppc_state.pc != 0xfff04158
+            && ppc_state.pc != 0xfff0415c
+            && ppc_state.pc != 0xfff04160
+            && ppc_state.pc != 0xfff04164
+            && ppc_state.pc != 0xfff04184
+            && ppc_state.pc != 0xfff04188
+            && ppc_state.pc != 0xfff0419c
+            && ppc_state.pc != 0xfff041a0
+            // PM8600 addresses
+            && ppc_state.pc != 0xfff0420c
+            && ppc_state.pc != 0xfff04210
+            && ppc_state.pc != 0xfff04214
+            && ppc_state.pc != 0xfff04218
+            && ppc_state.pc != 0xfff0421c
+            && ppc_state.pc != 0xfff04220
+            && ppc_state.pc != 0xfff04224
+            && ppc_state.pc != 0xfff04228
+            && ppc_state.pc != 0xfff04334
+            && ppc_state.pc != 0xfff04338
+            && ppc_state.pc != 0xfff0433c
+            && ppc_state.pc != 0xfff04340
+            && ppc_state.pc != 0xfff04360
+            && ppc_state.pc != 0xfff04364
+            && ppc_state.pc != 0xfff04378
+            && ppc_state.pc != 0xfff0437c
+        ) {
             static uint32_t last_phys_addr = -1;
             static uint32_t first_phys_addr = -1;
             if (phys_addr < last_phys_addr || phys_addr > last_phys_addr + 8) {
