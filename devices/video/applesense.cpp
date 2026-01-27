58bd293eb4f2d275494eb625f88f6a332e510ed4
joevt <joevt@shaw.ca>
2026-01-27 04:58:42 -0800

    Fixes for building with gcc.
    Co-Authored-By: Andrew Randrianasulu <randrianasulu@gmail.com>

--- devices/video/applesense.cpp
+++ devices/video/applesense.cpp
@@ -28,6 +28,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <map>
 #include <string>
 #include <array>
+#include <cstring>
 #include <algorithm>
 
 /** Mapping between monitor IDs and their sense codes. */
