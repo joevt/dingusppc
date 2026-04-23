a79ad5b84f0aa3bdb7617a728481f28466b2162e
joevt <joevt@shaw.ca>
2026-04-23 04:47:36 -0700

    nvidianv: Cleanup header and footer.

--- devices/video/nvidianv.h
+++ devices/video/nvidianv.h
@@ -1,12 +1,25 @@
 /*
 DingusPPC - The Experimental PowerPC Macintosh emulator
 Copyright (C) 2018-26 The DingusPPC Development Team
+          (See CREDITS.MD for more details)
+
+(You may also contact divingkxt or powermax2286 on Discord)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
 Ported from Bochs bx_geforce_c (Copyright 2025-2026 The Bochs Project, LGPL v2+).
 */
 
@@ -431,4 +444,3 @@ extern std::atomic<bool> nv_vblank;
 void start_nv_scanout();
 void stop_nv_scanout();
 // --------------------------------------
-
